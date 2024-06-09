8d1219deca2eae0abc681b28ecb20708ccccb5f6
ambassador74 <collabus@list.ru>
2024-06-09 21:59:14 +0300

    xml_object has been added

--- WeatherService.cpp
+++ WeatherService.cpp
@@ -1,7 +1,10 @@
 ﻿#include "JsonService.h"
+#include "XmlService.h"
 
 int main() {
 	JsonService js;
 	Weather w = js.getWeather("weather.json");
+	XmlService xs;
+	Weather w2 = xs.getWeather("weather.xml");
 	return 0;
 }
